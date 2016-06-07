490bac2f6de596b3e69893e27be00323f4f14b9d
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-06-07 15:02:31 +0100

    function to pick up tests matching "_test"
    and doc function nicked from ct_helper at ninenines

--- src/ct_common.erl
+++ src/ct_common.erl
@@ -1,10 +1,34 @@
 -module(ct_common).
 
--export([doc/1]).
+-export([all/1,
+         doc/1]).
 
 %%------------------------------------------------------------------------------
 %% public
 %%------------------------------------------------------------------------------
+
+all(Mod) ->
+    lists:map(fun fun_only/1, lists:filter(fun filter_test/1, Mod:module_info(exports))).
+
+fun_only({Fun, _}) -> Fun.
+
+filter_test({Fun, 1}) ->
+    is_test_fun(rev(atom_to_binary(Fun, utf8)));
+filter_test(_) ->
+    false.
+
+rev(Bin) ->
+    rev(Bin, <<>>).
+
+rev(<<>>, Acc) -> Acc;
+rev(<<H:1/binary, Rest/binary>>, Acc) ->
+    rev(Rest, <<H/binary, Acc/binary>>).
+
+is_test_fun(<<"tset_", _/binary>>) ->
+    true;
+is_test_fun(_) ->
+    false.
+
 
 doc(String) ->
     ct:comment(String),
